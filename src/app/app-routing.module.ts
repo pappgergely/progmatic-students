88566c39eaa8bdd656c3301425f6843437e652e2
Papp Gergely <papp.gergely@aurettoworks.com>
2020-07-06 15:06:58 +0200

    2020-07-06 haladásunk

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {StudentsComponent} from "./components/students/students.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'students', component: StudentsComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'students' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ todo.md
@@ -0,0 +1,5 @@
+# TODO
+* Hozzáad gomb - új oldalon űrlap
+* Módosít gomb - modal űrlap
+* Törlés gomb - + user confirmation
+* FEMALE, MALE a táblázatban
